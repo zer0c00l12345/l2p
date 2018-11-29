174101bfdaed0b511f6f98f868fcc49dd691d710
zer0c00l12345 <37095690+zer0c00l12345@users.noreply.github.com>
2018-11-29 14:04:50 -0800

    Add files via upload

---
+++ login2.py
@@ -0,0 +1,31 @@
+import getpass 
+import sys
+
+
+
+loginVerify = input(" Have you logged in before? ")
+if loginVerify == 'yes' or loginVerify =='Yes':
+	UserName = input(" Please Enter Your User Name ")
+else:
+	print(" Please Create an Account ")
+	sys.exit(0)
+
+
+UserNameArray = ['Dan']
+UserPasswordArray = ['1q']
+
+
+# Check user name against array
+if UserName in UserNameArray:
+	UserPassword = input
+	UserPassword = getpass.getpass(" Please Enter your Password ")
+else:
+	print(" Access Denied ") 
+
+# Check Password Against Array
+if UserPassword in UserPasswordArray:
+	print(" Access granted! ")
+else:
+	print(" Access Denied! ")
+
+
